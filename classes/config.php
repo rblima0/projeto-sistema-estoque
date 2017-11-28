58b00af3260c425bc39c9101790a364027c68995
Rodrigo Banci <rblima0@gmail.com>
2017-11-28 16:07:27 -0200

    Arrumando nome do Banco

--- classes/config.php
+++ classes/config.php
@@ -3,6 +3,6 @@
 
     define('DB_DRIVE', 'mysql');
     define('DB_HOSTNAME', '127.0.0.1');
-    define('DB_DATABASE', 'eestoque');
+    define('DB_DATABASE', 'estoque');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '123456');
